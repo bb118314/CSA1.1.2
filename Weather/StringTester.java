9706e9c64065f37f2cd6caef559268e0dd709e45
Benjamen Brooks <roflwhy@gmail.com>
2019-02-26 10:04:12 -0600

    Add files via upload

---
+++ Weather/StringTester.java
@@ -0,0 +1,8 @@
+public class StringTester
+{
+    public static void main()
+    {
+        String weatherCondition = "2 Hurricane";
+        System.out.println(WeatherConditionals.getWeatherAdvice("Snow"));
+    }
+}
